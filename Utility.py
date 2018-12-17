2d2d4fd5ffd1551c892d39a03f5dfc46e516b4e3
Michele <mikelefonty@gmail.com>
2018-12-17 17:12:02 +0100

    Primo commit

--- Utility.py
+++ Utility.py
@@ -19,7 +19,7 @@ La matrice M passata in input non viene modificata.
 def addBias(M):
 
     M_bias = np.ones((M.shape[0],M.shape[1]+1))
-    M_bias[:,1:] = M
+    M_bias[:,1:] = np.copy(M)
     return M_bias
 
 """
@@ -33,16 +33,21 @@ La matrice M non viene modificata.
 
 
 def removeBias(M):
-    M_prime = M[:,1:]
+    M_prime = np.copy(M[:,1:])
     return M_prime
 
 """
-Costruisce una matrice random di dimensione (n_rows * n_cols) i cui elementi sono
+Costruisce una matrice random di dimensione (n_rows * n_cols + 1) i cui elementi sono
 compresi nel range [range_start, range_end].
-Se fan_in == True, allora gli elementi sono compresi nell'intervallo [range_start / (n_cols -1), range_end / (n_cols-1)]
+
+----------------------------------
+Viene aggiunta una colonna per il bias.
+----------------------------------
+
+Se fan_in == True, allora gli elementi sono compresi nell'intervallo [range_start / n_cols, range_end / n_cols]
 Per le matrici dei pesi:
     n_rows = # elementi nel layer corrente 
-    n_cols = # elementi nel layer precedente + colonna del bias
+    n_cols = # elementi nel layer precedente
 
 Gli elementi sono generati a partire da una distribuzione uniforme sull'intervallo specificato.
 
@@ -51,19 +56,23 @@ NOTA: LA MATRICE DEI PESI CONTIENE GIA' IL BIAS????   ;)
 -------------------------
 
 :param n_rows : numero di righe
-:param n_cols : numero di colonne (compreso il bias!!)
+:param n_cols : numero di colonne
 :param fan_in : bool. Se True, allora viene applicato il fan_in.
 :param range_start : estremo di inizio dell'intervallo
 :param range_end : estremo di fine dell'intervallo 
 """
 
 
-def init_Weights(n_rows,n_cols,fan_in = False,range_start = -0.7,range_end = 0.7):
+def init_Weights(n_rows,n_cols,fan_in=False,range_start=-0.7,range_end=0.7):
+
+    assert range_start < range_end
+    assert n_rows > 0
+    assert n_cols > 0
 
     if fan_in:
-        M = np.random.uniform(range_start / (n_cols - 1), range_end / (n_cols - 1), (n_rows,n_cols))
+        M = np.random.uniform(range_start / n_cols, range_end / n_cols, (n_rows,n_cols + 1))
     else:
-        M = np.random.uniform(range_start, range_end, (n_rows,n_cols))
+        M = np.random.uniform(range_start, range_end, (n_rows,n_cols + 1))
 
     return M
 
@@ -116,51 +125,50 @@ DA IMPLEMENTARE... FORMULA CON MATRICI????
 
 def compute_Accuracy_Regr(T,OUT):
     return NotImplementedError("Ancora non implementato!!!")
+
+
 """
-
-
+-------------------------------
 TEST VARI....
+------------------------------
 """
-X = np.array([
-    [2,3,4],
-    [2,3,4]
-])
+if __name__ == "__main__":
+    X = np.array([
+        [2,3,4],
+        [2,3,4]
+    ])
 
-n_row = 4
-n_col = 10
+    n_row = 4
+    n_col = 3
 
-W = init_Weights(n_row,n_col,fan_in=True)
-print("W = ",W)
-print(W.shape)
+    W = init_Weights(n_row,n_col,fan_in=True,range_start=0.1,range_end=0.3)
+    print("W = ",W)
+    print(W.shape)
 
-T = np.array([
-    [1,0],
-    [0,2]
-])
+    T = np.array([
+        [1,0],
+        [0,2]
+    ])
 
-Y = np.array([
-    [0,1],
-    [2,1]
-])
+    Y = np.array([
+        [0,1],
+        [2,1]
+    ])
 
-# T - Y =
-# [1 -1
-# -2  1]
-# ==> NORM(T-Y) = 1+1+4+1 = 7
 
-print("Error =", compute_Error(T,Y))
+    print("Error =", compute_Error(T,Y))
 
-T = np.array([
-    [1],
-    [0],
-    [1],
-    [1]
-])
+    T = np.array([
+        [1],
+        [0],
+        [1],
+        [1]
+    ])
 
-Y = np.array([
-    [0],
-    [1],
-    [1],
-    [1]])
+    Y = np.array([
+        [0],
+        [1],
+        [1],
+        [1]])
 
-print("Accuracy =", compute_Accuracy_Class(T,Y))
+    print("Accuracy =", compute_Accuracy_Class(T,Y))
